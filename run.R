813b5b8f3238cf27d680c29a3d0d0647b7980eff
breckbaldwin <git@lingpipe.com>
2020-12-07 22:30:58 -0500

    catchup for sharing with others

--- run.R
+++ run.R
@@ -6,46 +6,37 @@ library("cmdstanr")
 
 # Simulate data with parameters we are trying to recover with one predictor
 
-# Basline rate of COVID admissions to hospital 20 days out
-true_base_infection_count_per_day_intercept <- 100
 
-# Odd logic, if you call 111 50% chance of covid hospital admittance in 20 days
-true_coefficient_111_call_count_beta <- .5 
-true_std_dev_sigma <- 20
+generate_data <- function() {
+  true_coefficient_111_call_count_beta <- .5 
+  true_std_dev_sigma <- 20
+  days_data_n <- 100
+  calls_111_x <- runif(days_data_n, 0, 1000)
+  cvd_hosp_20_d_y <- rnorm(days_data_n, 
+                     + true_coefficient_111_call_count_beta * calls_111_x, 
+                     true_std_dev_sigma)
 
-days_data_n <- 100
-calls_111_x <- runif(days_data_n, 0, 1000)
-cvd_hosp_20_d_y <- rnorm(days_data_n, 
-                   true_base_infection_count_per_day_intercept
-                   + true_coefficient_111_call_count_beta * calls_111_x, 
-                   true_std_dev_sigma)
+  stan_data <- list(DAYS_DATA_N = days_data_n, 
+                    calls_111_x = calls_111_x, 
+                    cvd_hosp_20_d_y = cvd_hosp_20_d_y)
+  cat(sprintf(paste("simulation parameters are:",
+                       "\ntrue_coefficient_111_call_count_beta=%.1f",
+                       "\ntrue_std_dev_sigma=%.1f",
+                       "\ndays_data_n=%d\n"),
+                 true_coefficient_111_call_count_beta,
+                 true_std_dev_sigma, 
+                 days_data_n))
+  
+  return(stan_data)
+}
 
-stan_data <- list(DAYS_DATA_N = days_data_n, 
-                  calls_111_x = calls_111_x, 
-                  cvd_hosp_20_d_y = cvd_hosp_20_d_y)
+compile_run <- function(data) {
+  model <- cmdstan_model("stan/naive_linear_regression.stan")
+  fit <- model$sample(data = data, output_dir = "output")
+  return(fit)
+}
 
-cat(sprintf(paste("simulation parameters are:",
-                  "\ntrue_base_infection_count_per_day_intercept=%.1f",
-                  "\ntrue_coefficient_111_call_count_beta=%.1f",
-                  "\ntrue_std_dev_sigma=%.1f",
-                  "\ndays_data_n=%d"),
-            true_base_infection_count_per_day_intercept,
-            true_coefficient_111_call_count_beta,
-            true_std_dev_sigma, 
-            days_data_n))
-
-model <- cmdstan_model("stan/naive_linear_regression.stan")
-fit <- model$sample(data = stan_data, output_dir = "output")
-print(paste("ran stan executable: ", model$exe_file()))
-print(fit$summary())
-
-cat(sprintf(paste("simulation parameters are:",
-                  "\ntrue_base_infection_count_per_day_intercept=%.1f",
-                  "\ntrue_coefficient_111_call_count_beta=%.1f",
-                  "\ntrue_std_dev_sigma=%.1f",
-                  "\ndays_data_n=%d"),
-            true_base_infection_count_per_day_intercept,
-            true_coefficient_111_call_count_beta,
-            true_std_dev_sigma, 
-            days_data_n))
+stan_data <- generate_data()
+fit <- compile_run(data = stan_data)
+print(fit)
 
--- stan/naive_linear_regression.stan
+++ stan/naive_linear_regression.stan
@@ -5,18 +5,15 @@ data {
 }
 
 parameters {
-  real base_infection_count_per_day_intercept; // intercept
   real coefficient_111_call_count_beta; // slope, predictor coefficient
   real<lower = 0> std_dev_sigma; // error scale
 }
 
 model {
-  base_infection_count_per_day_intercept ~ normal(0, 1); // priors
   coefficient_111_call_count_beta ~ normal(0, 1);
   std_dev_sigma ~ normal(0, 1);
   for (n in 1:DAYS_DATA_N) {
-    cvd_hosp_20_d_y[n] ~ normal(base_infection_count_per_day_intercept
-                                + coefficient_111_call_count_beta * calls_111_x[n], 
+    cvd_hosp_20_d_y[n] ~ normal(coefficient_111_call_count_beta * calls_111_x[n], 
                                 std_dev_sigma); // likelihood
   }
 }
